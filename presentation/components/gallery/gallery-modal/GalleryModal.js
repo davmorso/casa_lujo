998a97895fa878e8d03e71d096980e0302f65d4c
davmorso1 <davmorso1@gmail.com>
2025-11-04 19:11:31 +0100

    v121: se pone fijo repo

--- presentation/components/gallery/gallery-modal/GalleryModal.js
+++ presentation/components/gallery/gallery-modal/GalleryModal.js
@@ -60,6 +60,9 @@ export default class GalleryModal {
           // Detecta si está en producción (GitHub Pages /casa_lujo/)
           const isProd = window.location.pathname.includes('/casa_lujo/');
           const repoPath = isProd ? '../_gallery/galleryRepository.js' : '../../_gallery/galleryRepository.js';
+
+          repoPath = "../_gallery/galleryRepository.js";
+
           import(repoPath).then(module => {
             // Pasa window._casaData como i18n para asegurar acceso correcto
             this._galleryRepository = module.default ? new module.default(window._casaData) : new module.GalleryRepository(window._casaData);
